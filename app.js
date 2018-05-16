170a4bd1360eecad0799215d2e6f6118bf1753ad
EC2 Default User <ec2-user@ip-172-31-7-219.us-east-2.compute.internal>
2018-05-16 14:27:41 +0000

    fix PORT

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
 const CH_SECRET = 'bc7d4dcadfed59988d014082bd5a46ee'; //Channel Secretを指定
 const CH_ACCESS_TOKEN = 'lUZF7+QLCwbNu4dONhfI9V9Ov+BHIIYOT7LwAspY0+6rb/+AiNCfGR7tjtLUXzysKHiyxb81aYZQ4Mdl/WeWk1SofCWueoZtVH4SbKeMpKihhZRxwRsBb+MmuLwpzJBu1VYzdWf0R2FWbkOkqJMslAdB04t89/1O/w1cDnyilFU='; //Channel Access Tokenを指定
 const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 /**
  * httpリクエスト部分
@@ -85,6 +85,6 @@ http.createServer((req, res) => {
         res.end('success');
     });
 
-}).listen(process.env.PORT || 8080);
+}).listen(PORT);
 
 console.log(`Server running at ${PORT}`);
